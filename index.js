d34e99e15414dcfefb33e2acdb482a2b970c46bf
Jesus <chuygs12@gmail.com>
2023-01-28 09:54:18 -0600

    add the folder utils with the files config, logger and middleware and refactor index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-require('dotenv').config()
-const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -8,19 +9,17 @@ const Blog = require('./models/blog')
 
 mongoose.set('strictQuery', false)
 
-
-const mongoUrl = process.env.MONGODB_URI
-
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
   .then(result => {
-    console.log('connected to database')
+    logger.info('connected to database')
   })
   .catch(err => {
-    console.log(err)
+    logger.error(err)
   })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.get('/', (request, response) => {
     response.send('Home page')
@@ -42,10 +41,13 @@ app.post('/api/blogs', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
-    .catch(err => console.log(err.message))
+    .catch(err => logger.error(err.message))
 })
 
-const PORT = process.env.PORT   
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+const PORT = config.PORT   
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
